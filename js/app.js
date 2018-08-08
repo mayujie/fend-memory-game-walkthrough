8506242a283fae9abb94a209f8f7ad2f9cfb719c
Yujie <nickmayujie@hotmail.com>
2018-08-08 11:27:10 +0200

    Storing Cards in an Array 1.0

--- js/app.js
+++ js/app.js
@@ -1,17 +1,30 @@
 /*
  * Create a list that holds all of your cards
  */
-// cards array holds all cards
+// select the parent element deck
 const deck = document.querySelector('.deck');
-// loop to add event listeners to each card
+//add the card to a list of open cards Storing Cards in an Array
+let toggledCards = [];
+// classList get the class of the event target
 deck.addEventListener('click', event => {
 	const clickTarget = event.target;
 	if (clickTarget.classList.contains('card')) {
 		console.log("Im a card");
-		clickTarget.classList.toggle('open');
-		clickTarget.classList.toggle('show');
+		toggleCard(clickTarget);
+		addToggleCard(clickTarget);
 	}
 });
+//toggling into own function
+function toggleCard(clickTarget) {
+	//.toggle to turn on or off the classes when our click event fires
+	clickTarget.classList.toggle('open');
+	clickTarget.classList.toggle('show');
+};
+//push the clickTarget into the toggledCards array
+function addToggleCard(clickTarget) {
+	toggledCards.push(clickTarget);
+	console.log(toggledCards);
+}
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
